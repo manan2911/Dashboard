4197da8d43fc525f2cd8c5909f4c4813a1f5640f
manan2911 <mananbhatt2911@gmail.com>
2023-05-14 01:26:11 +0530

    themeSet

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+
+import { useTheme, Box, IconButton } from "@mui/material";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import darkModeOutlinedIcon from "@mui/icons-material/darkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => {
+  return <div>Topbar</div>;
+};
+
+export default Topbar;
